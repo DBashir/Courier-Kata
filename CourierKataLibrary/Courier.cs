5fab7baaac7e81276d3f8de7668ba06063f312d4
Deen Bashir <deen-bashir@hotmail.com>
2022-11-01 17:59:46 +0000

    Small discount test & logic added

--- CourierKataLibrary/Courier.cs
+++ CourierKataLibrary/Courier.cs
@@ -6,6 +6,8 @@ namespace CourierKataLibrary
     {
         public string processParcel(Parcel[] parcels, bool speedyShipping)
         {
+            smallParcelDiscount(parcels);
+
             StringBuilder sb = new StringBuilder();
             int totalCost = 0;
             for(int i = 0; i < parcels.Length; i++)
@@ -22,5 +24,22 @@ namespace CourierKataLibrary
             return sb.ToString();
         }
 
+        private void smallParcelDiscount(Parcel[] parcels)
+        {
+            int Counter = 0;
+            for (int i = 0; i < parcels.Length; i++)
+            {
+                if (i == 0) Counter = 1;
+                Counter++;
+                if (parcels[i].ParcelType == ParcelType.Small)
+                {
+                    if (Counter % 4 == 0) parcels[i].Cost = 0;
+                }
+            }
+            return;
+
+        }
+
     }
+
 }
---
+++ CourierKataTest/DiscountTest.cs
@@ -0,0 +1,25 @@
+﻿using CourierKataLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourierKataTest
+{
+    [TestClass]
+    public class DiscountTest
+    {
+        [TestMethod]
+        public void fourthSmallParcelFreeTest()
+        {
+            Parcel smallParcel = new Parcel(1, 1, 1, 0);
+            Parcel smallParcel2 = new Parcel(1, 1, 1, 0);
+            Parcel smallParcel3 = new Parcel(1, 1, 1, 0);
+            Parcel smallParcel4 = new Parcel(1, 1, 1, 0);
+            Courier courier = new Courier();
+            Parcel[] parcels = { smallParcel, smallParcel2, smallParcel3, smallParcel4 };
+            Assert.AreEqual(courier.processParcel(parcels, false), "Small Parcel: $3Small Parcel: $3Small Parcel: $0Small Parcel: $3. Total Cost: $9");
+        }
+    }
+}
